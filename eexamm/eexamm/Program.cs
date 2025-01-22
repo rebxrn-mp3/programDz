c3266da6bdd3f84dc8cd2b9c12be827092a85101
tfeda <tfeda2005@icloud.com>
2025-01-22 13:18:53 +0500

    3

--- eexamm/eexamm/Program.cs
+++ eexamm/eexamm/Program.cs
@@ -10,6 +10,29 @@ namespace eexamm
     {
         static void Main(string[] args)
         {
+            int n = 32;
+            long result = CalculateS(n);
+            Console.WriteLine($"S({n}) = {result}");
+            Console.ReadKey();
+        }
+
+        static long CalculateS(int n)
+        {
+
+            long[] M = new long[n + 1];
+
+            M[1] = 2;
+            if (n >= 2)
+            {
+                M[2] = 6;
+            }
+
+            for (int i = 3; i <= n; i++)
+            {
+                M[i] = M[i - 1] * 2 + 2 * (i - 1);
+            }
+
+            return M[n];
         }
     }
 }
