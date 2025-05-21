4a8dc753f6aca3aebe4bdcc4b48453106b8b4981
tfeda <tfeda2005@icloud.com>
2025-05-21 19:59:12 +0500

    Задание 13

---
+++ WarehouseLibrary/Commodity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+namespace WarehouseLibrary
+{
+    public class Commodity
+    {
+        // Свойства, передаваемые через конструктор (отмечены точкой в задании)
+        public string Name { get; set; }                // Наименование
+        public double Weight { get; set; }              // Вес (кг)
+        public string Dimensions { get; set; }          // Габариты (длина x ширина x высота)
+        public DateTime ArrivalDate { get; set; }       // Дата поступления
+        public decimal Price { get; set; }              // Цена
+        public int MaxStack { get; set; }               // Особенности складирования (макс. стопок)
+
+        // Свойства только для чтения
+        public string Article { get; }                  // Артикул (только для чтения)
+        public CommodityCharacteristic Characteristic { get; } // Характеристика (перечисление)
+
+        // Конструктор
+        public Commodity(string article, string name, double weight, string dimensions,
+                         DateTime arrivalDate, decimal price, CommodityCharacteristic characteristic,
+                         int maxStack)
+        {
+            Article = article;
+            Name = name;
+            Weight = weight;
+            Dimensions = dimensions;
+            ArrivalDate = arrivalDate;
+            Price = price;
+            Characteristic = characteristic;
+            MaxStack = maxStack;
+        }
+
+        // Метод GetInfo()
+        public virtual string[] GetInfo()
+        {
+            return new string[]
+            {
+        $"{Article} - {Name}",
+        $"Вес: {Weight.ToString(CultureInfo.InvariantCulture)} кг. Характеристика: {GetCharacteristicText()}. Макс. стопок: {MaxStack}"
+            };
+        }
+
+        // Вспомогательный метод для перевода перечисления в текст
+        private string GetCharacteristicText()
+        {
+            switch (Characteristic)
+            {
+                case CommodityCharacteristic.Ordinary:
+                    return "Обыкновенный";
+                case CommodityCharacteristic.Fragile:
+                    return "Хрупкий";
+                case CommodityCharacteristic.MoistureSensitive:
+                    return "Боится сырости";
+                default:
+                    return "Неизвестно";
+            }
+        }
+    }
+}
---
+++ WarehouseLibrary/CommodityCharacteristic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseLibrary
+{
+    public enum CommodityCharacteristic
+    {
+        Ordinary,   // Обыкновенный
+        Fragile,    // Хрупкий
+        MoistureSensitive // Боится сырости
+    }
+}
